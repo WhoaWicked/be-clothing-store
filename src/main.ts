2110e2fbd2ddd10dd80659ef7af627fccfefd2db
WhoaWicked <9uy0232@gmail.com>
2025-12-20 23:16:11 +0700

    fix: restore server startup logic in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,19 @@ import app from './app';
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}.`);
-// });
-if (require.main === module || process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-        try {
-            // scheduleOverdueCheck();
-            // scheduleSoonOverdueCheck();
-        } catch (error) {
-            console.error("Error initializing scheduled tasks:", error);
-        }
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+});
+// if (require.main === module || process.env.NODE_ENV !== 'production') {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//         try {
+//             // scheduleOverdueCheck();
+//             // scheduleSoonOverdueCheck();
+//         } catch (error) {
+//             console.error("Error initializing scheduled tasks:", error);
+//         }
+//     });
+// }
 
 export default app;
